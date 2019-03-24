1783a30975e471040fa66a5d9d304d8f320c0ca0
Volodymyr Maievskyi <volodymyr.maievskyi@kindgeek.com>
2019-03-24 20:56:35 +0200

    Initial commit

---
+++ GeekPod/GeekPod.h
@@ -0,0 +1,19 @@
+//
+//  GeekPod.h
+//  GeekPod
+//
+//  Created by Volodymyr Maievskyi on 3/24/19.
+//  Copyright © 2019 Volodymyr Maievskyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GeekPod.
+FOUNDATION_EXPORT double GeekPodVersionNumber;
+
+//! Project version string for GeekPod.
+FOUNDATION_EXPORT const unsigned char GeekPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GeekPod/PublicHeader.h>
+
+
